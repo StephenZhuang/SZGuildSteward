7d3a345022a8ae2c61ec599bb356a18a6da1a6b2
Stephen <379128008@qq.com>
2015-04-20 14:31:06 +0800

    Init

---
+++ SZGuildSteward/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GVUserDefaults
+#define COCOAPODS_POD_AVAILABLE_GVUserDefaults
+#define COCOAPODS_VERSION_MAJOR_GVUserDefaults 0
+#define COCOAPODS_VERSION_MINOR_GVUserDefaults 9
+#define COCOAPODS_VERSION_PATCH_GVUserDefaults 5
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 1
+
+// ObjectiveRecord
+#define COCOAPODS_POD_AVAILABLE_ObjectiveRecord
+#define COCOAPODS_VERSION_MAJOR_ObjectiveRecord 1
+#define COCOAPODS_VERSION_MINOR_ObjectiveRecord 5
+#define COCOAPODS_VERSION_PATCH_ObjectiveRecord 0
+
+// ObjectiveSugar
+#define COCOAPODS_POD_AVAILABLE_ObjectiveSugar
+#define COCOAPODS_VERSION_MAJOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_MINOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_PATCH_ObjectiveSugar 0
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 2
+
+// SDWebImage/Core
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Core
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Core 2
+
---
+++ SZGuildSteward/SZGuildSteward/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SZGuildSteward
+//
+//  Created by Stephen Zhuang on 15/4/20.
+//  Copyright (c) 2015年 Stephen Zhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
